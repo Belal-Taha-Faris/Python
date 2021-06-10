df5f3b33e58b8d5663361168a426443aa14c4915
Belal Taha <bt2970918@gmail.com>
2021-06-10 23:16:25 +0200

    print avg
    take 3 numbers and print the avg

---
+++ Python-Print-Avg/main.py
@@ -0,0 +1,9 @@
+n1 = int(input("Enter three numbers to get the Average\n "))
+n2 = int(input())
+n3 = int(input())
+avg = ((n1+n2+n3)/3)
+print("The Avg of the taken numbers is : ", avg)
+#OR
+print("The Avg of the taken numbers is : ", ((n1+n2+n3)/3))
+#OR
+print(f"The Avg of the taken numbers is :  {avg} " )
