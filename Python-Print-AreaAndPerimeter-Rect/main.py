10f17ea5e4416b54a32fa365d1751414f086dd98
Belal Taha <bt2970918@gmail.com>
2021-06-11 12:19:25 +0200

    print rectangle area, perimeter
    takes input for l and w

---
+++ Python-Print-AreaAndPerimeter-Rect/main.py
@@ -0,0 +1,6 @@
+l = float(input("Please enter length for rectangle : "))
+w = float(input("Please enter width for rectangle : "))
+area = l*w
+perimeter = 2*(l+w)
+print("The area of rectangle is : ", area)
+print("The perimeter of rectangle is : %.2f" % perimeter)
